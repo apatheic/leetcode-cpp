1335f5f323fc95217777bd87737419d9cd274f27
apatheic <apatheic@proton.me>
2026-06-09 22:33:45 +0400

    Create FindTheDegreeOfEachVertex.cpp

---
+++ Easy/FindTheDegreeOfEachVertex.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<int> answer(n, 0);
+        for (int i = 0; i<n; ++i){
+            for (int j = 0; j < n; ++j){
+                answer[i] = answer[i] + matrix[i][j];    
+            }
+        }
+    return answer;    
+    }
+};
