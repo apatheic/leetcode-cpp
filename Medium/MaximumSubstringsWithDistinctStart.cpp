eb3459c23163cb405636c2978ae56e3f89722d35
apatheic <apatheic@proton.me>
2026-06-16 22:01:33 +0400

    Create MaximumSubstringsWithDistinctStart.cpp

---
+++ Medium/MaximumSubstringsWithDistinctStart.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        vector<bool> seen(26, false);
+        int count = 0;
+
+        for (char c : s) {
+            int index = c - 'a';
+            if (!seen[index]) {
+                seen[index] = true;
+                count++;
+            }
+        }
+        return count;
+    }
+};
